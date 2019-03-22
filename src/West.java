a8433ea01e4c3834deb3985a1b708093f47d923f
diba <diba.kheirkhah@gmail.com>
2019-03-22 23:19:21 +0100

    west direction

---
+++ src/West.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class West implements Direction {
+
+    private FileWriter writer = new FileWriter();
+
+    @Override
+    public String getName() {
+        return "West";
+    }
+
+
+    @Override
+    public Coordinate calculate(Coordinate coordinate, int steps) {
+
+        for (int i = 0; i < steps; i++) {
+            int currentX = coordinate.getX() - 1;
+            coordinate.setX(currentX);
+
+            String s = coordinate.toString();
+//            writer.append(List.of(s), "text_answer.txt");
+            writer.append(List.of(s), "/home/hosam/programming/coding-contest/coding-contest/src/text_answer.txt");
+
+        }
+
+        return coordinate;
+    }
+}
